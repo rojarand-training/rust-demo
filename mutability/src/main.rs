133d6612f1f8edb4426feb87ae5094c1983376fc
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2021-02-27 17:46:48 +0000

    Add mutability example

---
+++ mutability/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    //https://doc.rust-lang.org/book/ch03-02-data-types.html
+    let mut mutable_variable = 4;
+    mutable_variable = mutable_variable+2;
+    let immutable_variable = 4;
+
+    println!("mutable_variable={}, immutable_variable={}", mutable_variable, immutable_variable);
+    //immutable_variable = 6;
+
+    //Note!!! char is 4 butes in size
+    let x: char = '😻';
+    println!("Char x={}!", x);
+}
