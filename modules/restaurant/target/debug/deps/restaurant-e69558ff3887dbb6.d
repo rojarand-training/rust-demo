7ec099fa2206b00143cd3d78df3f85cbce90d895
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2021-04-18 06:48:05 +0000

    Move restaurant project to modules dir

---
+++ modules/restaurant/target/debug/deps/restaurant-e69558ff3887dbb6.d
@@ -0,0 +1,7 @@
+/home/zebul/projects/sandbox/rust/rust-demo/restaurant/target/debug/deps/restaurant-e69558ff3887dbb6: src/lib.rs src/back_of_house.rs src/back_of_house/provisioning.rs
+
+/home/zebul/projects/sandbox/rust/rust-demo/restaurant/target/debug/deps/restaurant-e69558ff3887dbb6.d: src/lib.rs src/back_of_house.rs src/back_of_house/provisioning.rs
+
+src/lib.rs:
+src/back_of_house.rs:
+src/back_of_house/provisioning.rs:
---
+++ modules/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/zebul/projects/sandbox/rust/rust-demo/restaurant/target/debug/librestaurant.rlib: /home/zebul/projects/sandbox/rust/rust-demo/restaurant/src/lib.rs
